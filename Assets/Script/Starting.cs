0ee593d658f2eb60921bdfc3673b60ba4986f77e
Apeksha Darbari <apekshadarbari@gmail.com>
2015-09-19 22:54:56 -0700

    Initial screens working - First Commit

---
+++ Assets/Script/Starting.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Starting : MonoBehaviour {
+
+	private GameObject infoBtn;
+	private GameObject startBtn;
+
+	private GameObject startImg;
+	private GameObject infoImg;
+
+
+
+	void Start () {
+		infoBtn = GameObject.Find("InfoBtn");
+		startBtn = GameObject.Find("StartBtn");
+
+		startImg =GameObject.Find("StartImage");
+		infoImg = GameObject.Find("InfoImage");
+
+		startImg.GetComponent<UITexture> ().enabled = true;
+		infoImg.GetComponent<UITexture> ().enabled = true;
+
+	}
+	
+	public void clickInfoBtn(){
+		Application.LoadLevel("Scene2");
+	//	startImg.GetComponent<UITexture> ().enabled = false;
+	//	infoImg.GetComponent<UITexture> ().enabled = true;
+	//	infoBtn.SetActive (false);
+	}
+
+
+
+	public void clickStartBtn(){
+		Application.LoadLevel("Scene3");
+
+	}
+}
